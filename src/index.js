39a3fdede30b4f457f83a8a021c7bb0574a886b5
Mayank Bansal <bmayank6789@gmail.com>
2026-06-20 10:49:28 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@
 // import DB_NAME from './constants'
 import connectDB from './db/index.js';
 import dotenv from "dotenv"
-
+import {app} from "./app.js"
 dotenv.config({
     path:"./env"
-})
+}) 
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+            console.log("Not Able to Connect with DB")
+            throw error
+        })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("DBMONGO CONNECTION FAILED ||", error)
+})
 
 
 
