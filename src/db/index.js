3b30919fced4f30aba01e620ba1f045950095c7c
Mayank Bansal <bmayank6789@gmail.com>
2026-07-02 12:15:43 +0530

    fix mongo uri db name insertion

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,14 @@ import { DB_NAME } from '../constants.js'
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        const uri = process.env.MONGO_URI
+        const [baseUri, queryString] = uri.split("?")
+        const cleanBase = baseUri.endsWith("/") ? baseUri.slice(0, -1) : baseUri
+        const finalUri = queryString
+            ? `${cleanBase}/${DB_NAME}?${queryString}`
+            : `${cleanBase}/${DB_NAME}`
+
+        const connectionInstance = await mongoose.connect(finalUri)
         console.log(`[DB] ✅ MongoDB connected | Host: ${connectionInstance.connection.host}`)
     }
     catch (error) {
